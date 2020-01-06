4a80fc0f7bba3c9dedd72f96f6daffacf8951f56
Brewer Kight <bkight@atlassian.com>
2020-01-06 01:29:19 -0600

    -This is the working copy part 2

---
+++ src/main/java/org/launchcode/springSecurity/UserPrincipalDetailService.java
@@ -0,0 +1,24 @@
+package org.launchcode.springSecurity;
+
+import org.launchcode.springSecurity.db.UserRepository;
+import org.launchcode.springSecurity.model.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserPrincipalDetailService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public UserPrincipalDetailService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(s);
+        UserPrincipal userPrincipal = new UserPrincipal(user);
+        return userPrincipal;
+    }
+}
